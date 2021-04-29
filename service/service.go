0b763111186b40e0dcd3ac24382c7f1ff067e2c5
savio <savio@localhost.localdomain>
2021-04-29 13:28:21 -0400

    negative expenses, posivite income

--- service/service.go
+++ service/service.go
@@ -7,6 +7,13 @@ import (
 )
 
 func CreateTransaction(transaction domain.Transaction) *utils.ApplicationError {
+
+	if (transaction.OperationType.Id == 1 ||
+		transaction.OperationType.Id == 2 ||
+		transaction.OperationType.Id == 3) && (transaction.Amount < 0) {
+		transaction.Amount = (transaction.Amount * -1)
+	}
+
 	return dao.CreateTransaction(transaction)
 
 }
